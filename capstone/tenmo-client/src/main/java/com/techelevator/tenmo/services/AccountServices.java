9aaee06c072778f329b34344b9fc2c4752df86a5
Hassanali Momin <hmomin2001@gmail.com>
2022-04-28 09:19:00 -0400

    cleaning up

--- capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountServices.java
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountServices.java
@@ -3,6 +3,7 @@ package com.techelevator.tenmo.services;
 import com.techelevator.tenmo.App;
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.AuthenticatedUser;
+import com.techelevator.tenmo.model.User;
 import com.techelevator.util.BasicLogger;
 import org.springframework.http.*;
 import org.springframework.web.client.ResourceAccessException;
@@ -10,6 +11,8 @@ import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountServices {
 
@@ -18,26 +21,25 @@ public class AccountServices {
     private String API_BASE_URL;
 
 
-
     public AccountServices(String url, AuthenticatedUser authenticatedUser) {
         this.authenticatedUser = authenticatedUser;
         this.API_BASE_URL = url;
     }
 
 
-    public BigDecimal returnBalance (){
-    BigDecimal balance = new BigDecimal(0);
-    try {
+    public BigDecimal returnBalance() {
+        BigDecimal balance = new BigDecimal(0);
+        try {
             balance = restTemplate.exchange(API_BASE_URL + "balance/", HttpMethod.GET, makeAuthEntity(), BigDecimal.class).getBody();
-    } catch (RestClientResponseException | ResourceAccessException e) {
-        BasicLogger.log(e.getMessage());
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return balance;
+
     }
-    return balance;
-
-}
 
 
-    private HttpEntity<Account> makeAccountEntity (Account account) {
+    private HttpEntity<Account> makeAccountEntity(Account account) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(authenticatedUser.getToken());
@@ -49,10 +51,6 @@ public class AccountServices {
         headers.setBearerAuth(authenticatedUser.getToken());
         return new HttpEntity<>(headers);
     }
-
-
-
-
 
 
 }
